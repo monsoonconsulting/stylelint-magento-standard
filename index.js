c5289fb992e4a35962b00b463be1915aa2413d06
Ross McHugh <rossmchugh@gmail.com>
2019-04-08 19:22:35 +0200

    updated selector-class-pattern to support BEM

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ module.exports = {
     indentation: 4,
     "string-quotes": "single",
     "selector-max-id": 0,
-    "selector-class-pattern": "^_?[a-z0-9]+(-[a-z0-9]+)*$",
+    "selector-class-pattern": "^_?[a-z0-9]+((-|--|__)[a-z0-9]+)*$",
     "max-nesting-depth": 4,
     "number-leading-zero": "never",
     "max-empty-lines": 2,
